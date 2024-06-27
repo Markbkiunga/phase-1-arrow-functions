e524fd4fd01b544825ac34d3deab77b4862c635e
Mark Brian <markbkiunga2017@gmail.com>
2024-06-27 14:01:04 +0300

    Completes lab

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function (num1 = 2000, num2 = 100) {
+  console.log(num1 / num2);
+  return num1 / num2;
+};
+// divide();
+
+const square = (num) => num ** 2;
+const add = (a, b) => a + b;
